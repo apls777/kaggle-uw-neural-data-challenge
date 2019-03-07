randomly rotate image to a fixed angle; batch normalization, dropout and L2 regularization for convolutional layers; added "logits" layer to the configuration; changed radians to degrees for the rotate distortion

uwndc19/helpers/image_distortion_funcs.py
@@ -1,3 +1,4 @@
+from math import pi
 import tensorflow as tf
 from tensorflow.python.ops import random_ops
 
@@ -21,11 +22,20 @@ def brightness(image, max_factor: float):
     return image
 
 
-def rotate(image, angle: float, interpolation: str):
-    assert angle > 0
+def rotate(image, max_angle: int, interpolation: str = 'BILINEAR'):
+    assert 0 < max_angle <= 180
     assert interpolation in ['NEAREST', 'BILINEAR']
 
-    rotate_angle = random_ops.random_uniform([], -angle, angle)
-    image = tf.contrib.image.rotate(image, rotate_angle, interpolation=interpolation)
+    max_angle_radians = tf.cast(max_angle, dtype=tf.float32) * pi / 180
+    rotate_angle_radians = random_ops.random_uniform([], -max_angle_radians, max_angle_radians)
 
-    return image
+    return tf.contrib.image.rotate(image, rotate_angle_radians, interpolation=interpolation)
+
+
+def rotate_choice(image, angles: list, interpolation: str = 'BILINEAR'):
+    assert interpolation in ['NEAREST', 'BILINEAR']
+
+    rotate_angle = tf.random.shuffle(angles)[0]
+    rotate_angle_radians = tf.cast(rotate_angle, dtype=tf.float32) * pi / 180
+
+    return tf.contrib.image.rotate(image, rotate_angle_radians, interpolation=interpolation)

uwndc19/models/layers.py
@@ -2,15 +2,29 @@ from functools import reduce
 import tensorflow as tf
 
 
-def build_conv_layers(input_tensor, conv_layers_params: list):
+def build_conv_layers(input_tensor, conv_layers_params: list, is_training: bool):
     for layer_params in conv_layers_params:
+        dropout_rate = layer_params.get('dropout_rate', 0)
+        if dropout_rate:
+            input_tensor = tf.layers.dropout(inputs=input_tensor, rate=dropout_rate, training=is_training)
+
+        l2_regularization = layer_params.get('l2_regularization', 0)
+        kernel_regularizer = None
+        if l2_regularization:
+            kernel_regularizer = tf.contrib.layers.l2_regularizer(scale=l2_regularization)
+
         input_tensor = tf.layers.conv2d(
             inputs=input_tensor,
             filters=layer_params['num_filters'],
             kernel_size=[layer_params['kernel_size'], layer_params['kernel_size']],
             padding=layer_params['padding'],
+            kernel_regularizer=kernel_regularizer,
             activation=tf.nn.relu)
+
         input_tensor = tf.layers.max_pooling2d(inputs=input_tensor, pool_size=[2, 2], strides=2)
+
+        if layer_params.get('batch_norm', False):
+            input_tensor = tf.layers.batch_normalization(input_tensor, training=is_training)
 
     flat_dim = reduce(lambda x, y: x * y, input_tensor.get_shape()[1:])
     flat = tf.reshape(input_tensor, [-1, flat_dim])

uwndc19/models/multiclass/model.py
@@ -9,7 +9,7 @@ def model_fn(features, labels, mode, params):
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
 
     # build convolutional layers
-    conv = build_conv_layers(image, params['model']['conv_layers'])
+    conv = build_conv_layers(image, params['model']['conv_layers'], is_training)
 
     # load convolutional and dense layers from a checkpoint
     freeze_variables = {}
@@ -35,22 +35,21 @@ def model_fn(features, labels, mode, params):
         if subnet_dropout_rate:
             dense = tf.layers.dropout(inputs=dense, rate=subnet_dropout_rate, training=is_training)
 
+        logits_layer_params = dict(params['model']['logits_layer'])
+        logits_layer_params['num_units'] = 1
+
         logits_concat = []
         for i in range(num_classes):
             subnet_dense = build_dense_layers(dense, params['model']['subnet']['dense_layers'], is_training)
-            logits_dropout_rate = params['model'].get('logits_dropout_rate', 0)
-            if logits_dropout_rate:
-                subnet_dense = tf.layers.dropout(inputs=subnet_dense, rate=logits_dropout_rate, training=is_training)
-            subnet_logits = tf.layers.dense(inputs=subnet_dense, units=1, activation=tf.nn.relu)
+            subnet_logits = build_dense_layers(subnet_dense, [logits_layer_params], is_training)
             logits_concat.append(subnet_logits)
 
         logits = tf.concat(logits_concat, axis=-1)
     else:
         # a single layer to get a spike
-        logits_dropout_rate = params['model'].get('logits_dropout_rate', 0)
-        if logits_dropout_rate:
-            dense = tf.layers.dropout(inputs=dense, rate=logits_dropout_rate, training=is_training)
-        logits = tf.layers.dense(inputs=dense, units=num_classes, activation=tf.nn.relu)
+        logits_layer_params = dict(params['model']['logits_layer'])
+        logits_layer_params['num_units'] = num_classes
+        logits = build_dense_layers(dense, [logits_layer_params], is_training)
 
     # return prediction specification
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -82,6 +81,11 @@ def model_fn(features, labels, mode, params):
             summaries=['learning_rate', 'loss', 'gradients', 'gradient_norm'],
             variables=train_vars,
         )
+
+        # perform update ops for batch normalization
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        train_op = tf.group([train_op, update_ops])
+
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     # evaluation metrics

uwndc19/models/neuron_emb/model.py
@@ -9,7 +9,7 @@ def model_fn(features, labels, mode, params):
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
 
     # build convolutional layers
-    conv = build_conv_layers(image, params['model']['conv_layers'])
+    conv = build_conv_layers(image, params['model']['conv_layers'], is_training)
 
     # build dense layers
     dense = build_dense_layers(conv, params['model']['dense_layers'], is_training)  # [batch_size, units]
@@ -37,6 +37,11 @@ def model_fn(features, labels, mode, params):
             optimizer='Adam',
             summaries=['learning_rate', 'loss', 'gradients', 'gradient_norm'],
         )
+
+        # perform update ops for batch normalization
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        train_op = tf.group([train_op, update_ops])
+
         return tf.estimator.EstimatorSpec(mode=mode, loss=mse_loss, train_op=train_op)
 
     # evaluation metrics